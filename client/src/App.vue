7e222c11cb70e2452402a5202ea92651da0e13c6
MRVDH <info@maartenvandenhoven.com>
2019-06-22 09:26:42 +0200

    initial vue cli 3 commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,115 +1,31 @@
 <template>
-    <v-app id="app" v-bind:dark="darkTheme" v-cloak>
-        <CustomMenuLeft/>
-        <CustomHeader/>
-        <router-view/>
-        <CustomFooter/>
-        <v-snackbar
-            left
-            v-model="snackbar"
-            :color="snackbarOptions.color"
-            :timeout="snackbarOptions.timeout">
-            {{ snackbarOptions.text }}
-            <v-btn
-                flat
-                @click="snackbar = false">
-                Close
-            </v-btn>
-        </v-snackbar>
-    </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import CustomMenuLeft from '@/components/navigation/CustomMenuLeft';
-import CustomHeader from '@/components/navigation/CustomHeader';
-import CustomFooter from '@/components/navigation/CustomFooter';
-import OAuthService from '@/services/OAuthService';
-import EventBus from '@/services/EventBus';
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
 
-export default {
-    name: 'App',
-    data: () => {
-        return {
-            darkTheme: false,
-            snackbar: false,
-            snackbarOptions: {
-                color: '',
-                timeout: 5000,
-                text: ''
-            }
-        };
-    },
-    mounted () {
-        this.$root.loggedInUser = null;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-        if (localStorage.darkTheme) {
-            this.darkTheme = localStorage.darkTheme === 'true';
-        } else {
-            localStorage.darkTheme = false;
-            this.darkTheme = false;
-        }
-
-        document.getElementById('loading-text').remove();
-
-        EventBus.$emit('mnk:start-loading', 'isuserloggedin');
-        OAuthService.isUserLoggedIn().then((res) => {
-            if (!res.data.isAuthenticated) {
-                EventBus.$emit('mnk:start-loading', 'getrequesttoken');
-                OAuthService.getRequestToken().then((res) => {
-                    EventBus.$emit('mnk:oauth-request-token-received', res.data);
-                }).catch(() => {
-                    EventBus.$emit('mnk:message-error', 'Something went wrong while trying to get your OAuth request token.');
-                }).finally(() => {
-                    EventBus.$emit('mnk:stop-loading', 'getrequesttoken');
-                });
-            } else {
-                EventBus.$emit('mnk:start-loading', 'getUserDetails');
-                OAuthService.getUserDetails().then((res) => {
-                    this.$root.loggedInUser = res.data;
-                }).catch(() => {
-                    EventBus.$emit('mnk:message-error', 'Something went wrong while trying to get your user details.');
-                }).finally(() => {
-                    EventBus.$emit('mnk:stop-loading', 'getUserDetails');
-                });
-            }
-        }).catch(() => {
-            EventBus.$emit('mnk:message-error', 'Something went wrong while trying to check if you are logged in.');
-        }).finally(() => {
-            EventBus.$emit('mnk:stop-loading', 'isuserloggedin');
-        });
-
-        EventBus.$on('mnk:toggle-dark-theme', () => {
-            this.darkTheme = !this.darkTheme;
-        });
-        EventBus.$on('mnk:message-success', (text) => {
-            this.snackbar = true;
-            this.snackbarOptions.color = 'success';
-            this.snackbarOptions.text = text;
-        });
-        EventBus.$on('mnk:message-info', (text) => {
-            this.snackbar = true;
-            this.snackbarOptions.color = 'info';
-            this.snackbarOptions.text = text;
-        });
-        EventBus.$on('mnk:message-error', (text) => {
-            this.snackbar = true;
-            this.snackbarOptions.color = 'error';
-            this.snackbarOptions.text = text;
-        });
-    },
-    watch: {
-        darkTheme (newSetting) {
-            localStorage.darkTheme = newSetting;
-        }
-    },
-    components: {
-        CustomMenuLeft,
-        CustomHeader,
-        CustomFooter
-    }
-};
-</script>
-
-<style scoped>
-    [v-cloak] { display:none; }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
